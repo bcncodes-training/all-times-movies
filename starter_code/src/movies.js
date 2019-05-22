e9e15e8435659a993662180b9c6fcede85a221b8
adri <bubakill@gmail.com>
2019-05-22 14:23:48 +0200

    NO done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,28 @@
 /* eslint no-restricted-globals: 'off' */
+
+
 // Turn duration of the movies from hours to minutes 
+
+function turnHoursToMinutes(array) {
+  array.map(movie=> {
+    let duracion = movie.duration,
+        duracionSinLetras = duracion.replace(/[hmin]/g,' '),
+        separador = " ", // un espacio en blanco
+        arrayHorasMinutos = duracionSinLetras.split(separador),
+        horasAMinutos = arrayHorasMinutos[0]*60,
+        hMinutos = parseInt (horasAMinutos),
+        minutos = parseInt (arrayHorasMinutos[2]),
+        durTotal = hMinutos + minutos;
+
+        console.log(durTotal);
+
+
+  }); 
+
+}
+
+
+turnHoursToMinutes(movies);
 
 
 // Get the average of all rates with 2 decimals 
@@ -18,3 +41,6 @@
 
 
 // Best yearly rate average
+
+
+
